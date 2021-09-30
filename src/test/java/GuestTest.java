667e7db6fd2a48a48c682c1621f2c4bc7ae99c9a
Derek Bracken <derekbracken@gmail.com>
2021-09-30 11:33:50 +0100

    guest has name test passed

--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class GuestTest {
+import hotel.Guest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void before(){
+        guest = new Guest("Tom Smith");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Tom Smith", guest.getName());
+    }
 }
