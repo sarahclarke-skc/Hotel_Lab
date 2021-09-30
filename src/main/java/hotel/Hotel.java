465b163124f4b3f4bbe09f110f457f8d1aa05c2f
Derek Bracken <derekbracken@gmail.com>
2021-09-30 12:11:43 +0100

    Hotel getter and setter set up

--- src/main/java/hotel/Hotel.java
+++ src/main/java/hotel/Hotel.java
@@ -1,4 +1,33 @@
 package hotel;
 
+import rooms.Bedroom;
+import rooms.ConferenceRoom;
+
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel() {
+        this.bedrooms = new ArrayList<>();
+        this.conferenceRooms = new ArrayList<>();
+    }
+
+    public ArrayList<Bedroom> getBedrooms() {
+        return bedrooms;
+    }
+
+    public void addBedrooms(Bedroom bedroom) {
+        this.bedrooms.add(bedroom);
+    }
+
+    public ArrayList<ConferenceRoom> getConferenceRooms() {
+        return conferenceRooms;
+    }
+
+    public void addConferenceRooms(ConferenceRoom conferenceRoom) {
+        this.conferenceRooms.add(conferenceRoom);
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,34 @@
+import hotel.Hotel;
+import org.junit.Before;
+import org.junit.Test;
+import rooms.Bedroom;
+import rooms.ConferenceRoom;
+import rooms.RoomType;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+
+    Hotel hotel;
+    Bedroom bedroom;
+    ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before(){
+        hotel = new Hotel();
+        bedroom = new Bedroom(RoomType.DOUBLE, 2);
+        conferenceRoom = new ConferenceRoom(RoomType.EVENTROOM2);
+    }
+
+    @Test
+    public void bedroomListStartsAtZero(){
+        int result = hotel.getBedrooms().size();
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void conferenceRoomListStartsAtZero(){
+        int result = hotel.getConferenceRooms().size();
+        assertEquals(0, result);
+    }
 }
